1a1f730a5ad71c504f229cb01c521c752cff7939
Catur Setyono <catursetyono542@gmail.com>
2026-03-06 20:05:32 +0700

    Refactor quotes in astro.config.mjs to use consistent double quotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import node from '@astrojs/node';
-import tailwind from '@astrojs/tailwind';
+import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: node({
-    mode: 'standalone'
+    mode: "standalone",
   }),
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
